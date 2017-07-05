fd17bda1e526feb2bdd95ce9b1192e2e10e2dd36
Subbagari <hsubbagari@fusionalliance.com>
2017-07-05 12:08:45 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// To decorate a component we need atleast two properties.. Selector and template/templateUrl.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Harsha\'s test app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-@NgModule({
+// This is the Root module. This is the starting point module for an angular application.
+
+@NgModule({   // Harsha: All the properties within this NgModule is called MetaData
+  // Harsha: The declarations property is used to make components and
+  // directives and pipes available to your module that don't come from another module
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [ // Harsha: The imports module is used to bring in other modules that your module is going to need
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // Harsha: This is the entry point for the app code.
 })
 export class AppModule { }
