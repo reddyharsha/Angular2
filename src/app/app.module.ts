852a49ba53bf7963eb0d70b037856ce0125f4ea0
hsubbagari <harsha.sub@gmail.com>
2017-07-26 12:19:01 -0400

    Template Driven form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './media-item-list/category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
 
 // This is the Root module. This is the starting point module for an angular application.
 
@@ -13,10 +15,10 @@ import { CategoryListPipe } from './media-item-list/category-list.pipe';
   // Harsha: The declarations property is used to make components and
   // directives and pipes available to your module that don't come from another module
   declarations: [
-    AppComponent, MediaItemComponent, MediaItemListComponent, FavoriteDirective, CategoryListPipe
+    AppComponent, MediaItemComponent, MediaItemListComponent, FavoriteDirective, CategoryListPipe, MediaItemFormComponent
   ],
   imports: [ // Harsha: The imports module is used to bring in other modules that your module is going to need
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent] // Harsha: This is the entry point for the app code.
