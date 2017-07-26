10480707ad5194c1d4d999b83fb4ad29d59e7d4b
hsubbagari <harsha.sub@gmail.com>
2017-07-26 11:04:30 -0400

    Enhanced Favorite Directive

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,11 +1,23 @@
-import { Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appFavorite]'
 })
 
 export class FavoriteDirective {
-  @HostBinding('class.is-favorite') isFavorite = true;
+
+  @HostBinding('class.is-favorite') isFavorite = false;
+  
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+  
+  @HostListener('mouseenter') onMouseEnter() {
+    this.hovering = true;
+  }
+  
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hovering = false;
+  }
+   
   @Input() set appFavorite(value) {
     this.isFavorite = value;
   }
